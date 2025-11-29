7bab3f15e91704e901c9eafed0ccee594ddedd03
Apurba Singh <aprubansinghdev@gmail.com>
2025-11-29 21:36:03 +0600

    Servie provider update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
+use DB;
+use Event;
 use App\Models\SiteSetting;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +25,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-		view()->share('site_settings', SiteSetting::first());
+      include __DIR__ . '/../Http/Macros.php';
+
+      // current password validation rule
+      Validator::extend('current_password', function ($attribute, $value, $parameters, $validator) {
+        return Hash::check($value, Auth::user()->password);
+      });
+
+	    if (env('APP_ENV') === 'local') {
+	        DB::connection()->enableQueryLog();        
+	    }
+		  view()->share('site_settings', SiteSetting::first());
     }
 }
